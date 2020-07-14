d827c98e37078eaecc38afa4c603413e55cbe57c
Ryan Talbot <ryan-talbot@outlook.com>
2020-07-14 17:38:09 +0100

    Info and variables pulled and ready

---
+++ script.js
@@ -0,0 +1,17 @@
+const list_names = [
+  "John",
+  "Ben",
+  "James",
+  "David",
+  "Tom",
+  "Samuel",
+  "Sarah",
+  "Jane",
+  "Daniel",
+  "Samantha",
+];
+
+let rows = 3;
+let currentPage = 1;
+const ul = document.querySelector(".results");
+const btnWrapper = document.querySelector(".btn-wrapper");
