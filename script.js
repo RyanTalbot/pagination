a1d1b14ef651231a73bbdef733e3d0151cc6ebcf
Ryan Talbot <ryan-talbot@outlook.com>
2020-07-14 17:44:40 +0100

    Display list function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const list_names = [
+const listNames = [
   "John",
   "Ben",
   "James",
@@ -15,3 +15,25 @@ let rows = 3;
 let currentPage = 1;
 const ul = document.querySelector(".results");
 const btnWrapper = document.querySelector(".btn-wrapper");
+
+function DisplayList(names, wrapper, rowsPerPage, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rowsPerPage * page;
+  let end = start + rowsPerPage;
+
+  let paginatedNames = names.slice(start, end);
+
+  for (let i = 0; i < paginatedNames.length; i++) {
+    let name = paginatedNames[i];
+
+    let liElement = document.createElement("li");
+    liElement.classList.add("name");
+    liElement.textContent = name;
+
+    wrapper.appendChild(liElement);
+  }
+}
+
+DisplayList(listNames, ul, rows, currentPage);
